284714e498798c6a47a1ab0d67ab846d68f31e21
Daniel <bottom-official-70@inboxkitten.com>
2022-03-10 15:23:38 -0500

    add plant shooting behavior and projectile movement

---
+++ Assets/Scripts/Ennemy/PiranhaPlant/PlantBehavior.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlantBehavior : MonoBehaviour
+{
+    private RaycastHit2D _hit, _hitBehind;
+    [SerializeField]
+    private GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hit = Physics2D.Raycast(transform.position, Vector3.left, 5.5f);
+        _hitBehind = Physics2D.Raycast(transform.position, Vector3.right, 5.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        _hit = Physics2D.Raycast(transform.position, Vector3.left, 5.5f);
+        _hitBehind = Physics2D.Raycast(transform.position, Vector3.right, 5.5f);
+
+        if (_hit.collider != null && _hit.collider.CompareTag("Player"))
+        {
+            ShootProjectile();
+        }
+        if (_hitBehind.collider != null && _hitBehind.collider.CompareTag("Player"))
+        {
+            ShootProjectile();
+        }
+    }
+
+    private void ShootProjectile()
+    {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, _hit.point);
+        Gizmos.DrawLine(transform.position, _hitBehind.point);
+    }
+#endif
+}
---
+++ Assets/Scripts/Ennemy/PiranhaPlant/ProjectileMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.left * speed, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
