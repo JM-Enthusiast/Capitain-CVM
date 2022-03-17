1fffbcca5eb8370d610f3e133bd001ce1b6e12e8
Daniel <92190150+MagixFan3000@users.noreply.github.com>
2022-03-16 20:40:58 -0400

    add helmets unlock menu

---
+++ Assets/Scripts/Collectable et UI/HelmetMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelmetMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var helmets = GameManager.Instance.PlayerData.UnlockedHelmets;
+        foreach (var helmet in helmets)
+        {
+            GameObject.Find(helmet).transform.GetChild(1).GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+}
